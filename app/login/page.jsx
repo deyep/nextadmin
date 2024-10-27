3268fbf44afc769f500e90d716d378112ee0d0d6
kensuke.amaba <ideyep@gmail.com>
2024-10-27 11:16:21 +0900

    UI design complited

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,15 @@
-const Login = () => {
-  return <div>Login</div>;
+import styles from "@/app/ui/login/login.module.css";
+const LoginPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" placeholder="userName" />
+        <input type="password" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+export default LoginPage;
